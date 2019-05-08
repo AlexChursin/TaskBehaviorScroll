674337ce80ae43a80d2e44c3cd08d2e8ea846aa7
Alexander Marsin <aleksandr26rus25@gmail.com>
2019-05-09 02:54:09 +0300

    edited

---
+++ app/src/main/java/com/example/scrollingexample/activities/ScrollingActivity.kt
@@ -0,0 +1,54 @@
+package com.example.scrollingexample.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.example.scrollingexample.model.ProfilePeopleInList
+import com.example.scrollingexample.adapters.AdapterListPeople
+
+import kotlinx.android.synthetic.main.scrolling_activity.*
+import android.view.*
+import com.example.scrollingexample.R
+import com.example.scrollingexample.data.DataGenerator
+
+
+class ScrollingActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrolling_activity)
+        setSupportActionBar(prof_toolbar)
+
+        val sheetBottom = layoutInflater.run { inflate(R.layout.sheet_basic,null) }
+        val viewAdapter = AdapterListPeople(this,DataGenerator.getPeopleData(this), sheetBottom, bottom_sheet )
+        val viewManager = LinearLayoutManager(this)
+        prof_recycler_items.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+            isLongClickable = true
+            isClickable = true
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/scrollingexample/views/ScrollingView.kt
@@ -0,0 +1,4 @@
+package com.example.scrollingexample.views
+
+interface ScrollingView {
+}
